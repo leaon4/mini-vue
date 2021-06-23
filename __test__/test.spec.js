e9fe912d3a85718e1cb723b25727255dd6618906
leaon4 <leaon4@sina.com>
2021-06-23 22:04:25 +0800

    jest引入，项目搭建完成

---
+++ __test__/test.spec.js
@@ -0,0 +1,8 @@
+
+import { add } from '../src/add';
+
+describe('test', () => {
+    test('test', () => {
+        expect(add(3, 5)).toBe(8);
+    })
+})
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import _ from 'lodash';
+import { add } from './add';
 
 function component() {
     const element = document.createElement('div');
 
     // lodash 在当前 script 中使用 import 引入
-    element.innerHTML = _.join(['Hello', 'webpfdsfack'], ' ');
+    element.innerHTML = ['Hello', 'webpfdsfack', add(3, 5)].join(' ')
     console.log(element)
     console.log('haha')
     return element;
