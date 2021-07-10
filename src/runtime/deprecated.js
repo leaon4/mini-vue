0d857144648270b9a8bb56f875d9f68797f21b79
leaon4 <leaon4@sina.com>
2021-07-10 15:06:35 +0800

    算法复杂度降为O(n)

--- src/runtime/deprecated.js
+++ src/runtime/deprecated.js
@@ -1,5 +1,42 @@
+// 原始版本
+function patchKeyedChildren(c1, c2, container, anchor) {
+    let lastIndex = 0;
+    for (let i = 0; i < c2.length; i++) {
+        const next = c2[i];
+        let find = false;
+        for (let j = 0; j < c1.length; j++) {
+            const prev = c1[j];
+            if (prev.key === next.key) {
+                find = true;
+                patch(prev, next, container, anchor);
+                if (j < lastIndex) {
+                    const curAnchor = c2[i - 1].el.nextSibling;
+                    container.insertBefore(next.el, curAnchor);
+                } else {
+                    lastIndex = j;
+                }
+                break;
+            }
+        }
+        if (!find) {
+            const curAnchor = i === 0
+                ? c1[0].el
+                : c2[i - 1].el.nextSibling;
+            patch(null, next, container, curAnchor)
+        }
+    }
+    for (let i = 0; i < c1.length; i++) {
+        const prev = c1[i];
+        if (!c2.find(next => next.key === prev.key)) {
+            unmount(prev);
+        }
+    }
+}
+
+
 // 这个版本可以兼容没有key的情况。但没有意义
 function patchKeyedChildren(c1, c2, container, anchor) {
+    let lastIndex = 0;
     for (let i = 0; i < c2.length; i++) {
         const next = c2[i];
         if (next.key == null) {
@@ -9,7 +46,6 @@ function patchKeyedChildren(c1, c2, container, anchor) {
             patch(null, next, container, curAnchor)
             continue;
         }
-        let lastIndex = 0;
         let find = false;
         for (let j = 0; j < c1.length; j++) {
             const prev = c1[j];
